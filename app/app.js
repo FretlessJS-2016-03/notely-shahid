25fb269672815a9f20c5915726282509cd58bca1
Shahid Afzal <shahidafzal@hotmail.com>
2016-03-29 17:02:15 -0400

    Add notes controller

--- app/app.js
+++ app/app.js
@@ -1,22 +1,13 @@
 (function() {
   var notelyApp = angular.module('notely', [
-    'ui.router'
+    'ui.router',
+    'notely.notes'
   ]);
 
-  function config($stateProvider, $urlRouterProvider) {
+  function notelyConfig($urlRouterProvider) {
     $urlRouterProvider.otherwise('/notes');
-
-    $stateProvider
-
-      .state('notes', {
-        url: '/notes',
-        template: '<h1>Notely</h1><p>{{ message }}</p>',
-        controller: function($scope) {
-          $scope.message = "Welcome to Notely!";
-        }
-      });
   }
 
-  config.$inject = ['$stateProvider', '$urlRouterProvider'];
-  notelyApp.config(config);
+  notelyConfig.$inject = ['$urlRouterProvider'];
+  notelyApp.config(notelyConfig);
 })();
