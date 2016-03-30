869ea8b92ee007eb2867c6d9f593d609bc7d4244
Shahid Afzal <shahidafzal@hotmail.com>
2016-03-30 19:43:25 -0400

    Functionality to create notes from the client

--- server/app.js
+++ server/app.js
@@ -1,16 +1,35 @@
 var express = require('express');
 var notelyServerApp = express();
 var Note = require('./models/note');
+var bodyParser = require('body-parser');
+
+// Allow ourselves to use `req.body` to work with form data
+notelyServerApp.use(bodyParser.json());
 
 // Cross-Origin Resource Sharing (CORS) middleware
 notelyServerApp.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
 
-notelyServerApp.get('/', function(req, res) {
+notelyServerApp.get('/notes', function(req, res) {
   Note.find().then(function(notes) {
     res.json(notes);
+  });
+});
+
+notelyServerApp.post('/notes', function(req, res) {
+  var note = new Note({
+    title: req.body.note.title,
+    body_html: req.body.note.body_html
+  });
+
+  note.save().then(function(noteData) {
+    res.json({
+      message: 'Saved!',
+      note: noteData
+    });
   });
 });
 
