0b074adfa140673c87e451c037fb202145494066
Shahid Afzal <shahidafzal@hotmail.com>
2016-03-30 13:47:54 -0400

    Added mongo db integration

--- server/app.js
+++ server/app.js
@@ -1,25 +1,39 @@
 var express = require('express');
 var notelyServerApp = express();
 
-//Cross-origin resource sharing (CORS) middleware
-notelyServerApp.use(function(req, res, next){
+var db = require('mongoose');
+db.connect('mongodb://localhost:27017/notely');
+
+var NoteSchema = db.Schema({
+  title: String,
+  body_html: String
+});
+
+var Restaurant = db.model('Note', NoteSchema);
+
+// Cross-Origin Resource Sharing (CORS) middleware
+notelyServerApp.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   next();
-
-});
-notelyServerApp.get('/', function(req, res){
-res.json([
-  {
-    title: 'Edited Hardcoded note',
-    body_html: 'Cool note. Aqq, shucks.'
-  },
-  {
-    title: 'Another edited hardcoded note',
-    body_html: "Ain't life grand?"
-  }
-])
 });
 
-notelyServerApp.listen(3030, function(){
-console.log('Listing on http://localhost:3030');
+notelyServerApp.get('/', function(req, res) {
+  Restaurant.find().then(function(notes) {
+    res.json(notes);
+  });
+
+  // res.json([
+  //   {
+  //     title: 'Edited hardcoded note!',
+  //     body_html: 'Cool note. Aww, shucks.'
+  //   },
+  //   {
+  //     title: 'Another edited hardcoded note',
+  //     body_html: "Ain't life grand?"
+  //   }
+  // ]);
+});
+
+notelyServerApp.listen(3030, function() {
+  console.log('Listening on http://localhost:3030');
 });
