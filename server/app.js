76a021711aa19fbae8c519287948e8848f937038
Shahid Afzal <shahidafzal@hotmail.com>
2016-03-29 20:13:24 -0400

    Server static notes

--- server/app.js
+++ server/app.js
@@ -2,7 +2,16 @@ var express = require('express');
 var notelyServerApp = express();
 
 notelyServerApp.get('/', function(req, res){
-res.send('I <3 Express!')
+res.json([
+  {
+    title: 'Hardcoded note',
+    body_html: 'Cool note. Aqq, shucks.'
+  },
+  {
+    title: 'Another hardcoded note',
+    body_html: "Ain't life grand?"
+  }
+])
 });
 
 notelyServerApp.listen(3030, function(){
