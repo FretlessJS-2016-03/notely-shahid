d03dcf7a226e90e5366025a3119c8bc434e93e3a
Shahid Afzal <shahidafzal@hotmail.com>
2016-03-31 17:25:20 -0400

    Sanilitize body_html and save body_text

--- server/models/note-schema.js
+++ server/models/note-schema.js
@@ -1,13 +1,18 @@
 var db = require('../config/db');
+var sanitizeHtml = require('sanitize-html');
+var htmlToText = require('html-to-text');
 
 var NoteSchema = db.Schema({
   title: String,
   body_html: String,
+  body_text: String,
   updated_at: { type: Date, default: Date.now }
 });
 
 NoteSchema.pre('save', function(next) {
   this.updated_at = Date.now();
+  this.body_html = sanitizeHtml(this.body_html);
+  this.body_text = htmlToText.fromString(this.body_html);
   next();
 });
 
