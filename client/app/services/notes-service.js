acd8b16031ff2d3ffe2c57fba8537e3bba5dc3d5
Shahid Afzal <shahidafzal@hotmail.com>
2016-03-30 20:33:23 -0400

    Create connection from .env

--- client/app/services/notes-service.js
+++ client/app/services/notes-service.js
@@ -8,7 +8,7 @@
         _this.notes = [];
 
         _this.fetch = function() {
-          return $http.get('http://localhost:3030')
+          return $http.get('http://localhost:3030/notes')
             .then(function(response) {
               _this.notes = response.data;
             },
@@ -21,5 +21,14 @@
         _this.getNotes = function() {
           return _this.notes;
         };
+
+        _this.create = function (note){
+          return $http.post('http://localhost:3030/notes', {
+            note: note
+          }).then(function(response){
+            _this.notes.unshift(response.data.note);
+          })
+        };
+
       }
     }());
